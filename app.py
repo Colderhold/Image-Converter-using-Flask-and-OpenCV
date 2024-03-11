d379416748fdb2944002ff30317514c946028d14
Dhruv Shetty <dhruvshetty2502@gmail.com>
2024-03-11 19:26:49 +0530

    test 82

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, flash, send_file
 from PIL import Image
-import os, shutil
+import os
 import tempfile
 
 UPLOAD_FOLDER = 'uploads'
@@ -11,7 +11,8 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 'super secret key'
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def processImage(temp_filename, operation):
     print(f'The operation is {operation} and filename is {temp_filename}')
@@ -24,41 +25,20 @@ def processImage(temp_filename, operation):
         print('Error: Image not loaded.')
         return 'error'
 
-    base_filename, _ = os.path.splitext(os.path.basename(temp_filename))
+    base_filename, file_extension = os.path.splitext(os.path.basename(temp_filename))
 
     if operation == 'cgray':
         imgProcessed = img.convert('L')  # Convert to grayscale
-        output_extension = 'png'
     elif operation == 'cwebp':
         imgProcessed = img  # No need to process for webp
-        output_extension = 'webp'
-    elif operation == 'cjpg':
-        imgProcessed = img.convert('RGB')  # Convert to RGB
-        output_extension = 'jpg'
-    elif operation == 'cjpeg':
-        imgProcessed = img.convert('RGB')  # Convert to RGB
-        output_extension = 'jpeg'
-    elif operation == 'cpng':
-        imgProcessed = img  # No need to process for png
-        output_extension = 'png'
     else:
-        print('Error: Operation not recognized.')
-        return 'error'
+        imgProcessed = img.convert('RGB')  # Convert to RGB
 
     # Create a temporary file to store the processed image
-    _, temp_output_filename = tempfile.mkstemp(suffix=f'.{output_extension}')
+    _, temp_output_filename = tempfile.mkstemp(suffix=file_extension)
     imgProcessed.save(temp_output_filename)
 
-    # Create a new filename with the desired extension
-    final_output_filename = os.path.join(os.path.dirname(temp_filename), f'{base_filename}.{output_extension}')
-
-    # Copy the file to the new filename
-    shutil.copy2(temp_output_filename, final_output_filename)
-
-    # Remove the temporary file
-    os.remove(temp_output_filename)
-
-    return final_output_filename
+    return temp_output_filename
 
 @app.route('/')
 def home():
